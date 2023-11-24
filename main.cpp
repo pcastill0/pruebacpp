a5f5285bb30796fd24eadf8e6cd5d244b88a9d64
Pedro castillo <pedro.castillo@enti.cat>
2023-11-24 10:57:03 +0100

    Comentarios quitados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
 int main(int argc, char** argv ){
-	//std::cout << "Buenaaaas\n";
-
-	//std::cout << argc << std::endl;
+	
 	if(argc > 1){
 		std::cout << "Hola " << argv[1] << "!" << std::endl;
 	} else {
